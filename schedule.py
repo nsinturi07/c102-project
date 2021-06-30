09e45632df31a387666535eaa6bf9210c125936b
nsinturi07 <65034759+nsinturi07@users.noreply.github.com>
2021-06-29 19:49:55 -0500

    Add files via upload

---
+++ schedule.py
@@ -0,0 +1,20 @@
+day = input("whats day is it?:")
+if(day == 'mon'):
+
+    print("today you have math class")
+    
+elif(day == 'tue'):
+    print("today you have coding class in the evening")
+    
+elif(day == 'wed'):
+    print("today you have coding class in the evening and tennis in the morning")
+    
+elif(day == 'thu'):
+    print("today you have coding class in the evening and math class in the afternoon")
+    
+elif(day == 'fri'):
+    print('Today you have no classes')
+elif(day == 'sat'):
+    print('today you have math class in the morning')
+elif(day == 'sun'):
+    print('today you have coding class')
